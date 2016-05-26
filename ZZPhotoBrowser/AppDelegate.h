197b92494ef0e1a9523c88b070fada1f0e03254a
zziazm <1310726454@qq.com>
2016-05-26 16:00:37 +0800

    Initial Commit

---
+++ ZZPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZPhotoBrowser
+//
+//  Created by 赵铭 on 16/5/26.
+//  Copyright © 2016年 zziazm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
